4750c9117575af1585dc997ace77fb1d58094318
Philipp Walter <philippwalter@pm.me>
2022-03-31 00:20:21 +0200

    move inline content script, format files

--- background.js
+++ background.js
@@ -1,15 +1,25 @@
-//Show options and install message on first install only.
+// Show options and install message on first install only.
 function handleInstalled(details) {
-    if (details.reason == 'install') {
-        let createData = {
-            focused: true,
-            type: "popup",
-            url: "popup/install.html",
-            width: 365,
-            height: 750,
-        };
-        let creating = chrome.windows.create(createData);
-    }
+  if (details.reason == "install") {
+    let createData = {
+      focused: true,
+      type: "popup",
+      url: "popup/install.html",
+      width: 365,
+      height: 750,
+    };
+
+    chrome.windows.create(createData);
+  }
 }
 
 chrome.runtime.onInstalled.addListener(handleInstalled);
+
+chrome.runtime.onMessage.addListener((msg, sender) => {
+  if (msg.text == "runRemoveVideoAds") {
+    chrome.scripting.executeScript({
+      target: { tabId: sender.tab.id },
+      files: ["remove_video_ads.js"],
+    });
+  }
+});
